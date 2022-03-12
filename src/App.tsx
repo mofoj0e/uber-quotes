704a5470dea566488ecd34ed83780a090dffc989
Joe Nguyen <joetunguyen@gmail.com>
2022-03-11 18:22:32 -0600

    Add retrieval of quotes and background Add Axios Clean up css for background

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,54 @@
-import React from 'react';
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import axios, { AxiosResponse } from 'axios';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
-  );
+interface Quote {
+	author: string
+	authorSlug: string
+	content: string
+	dateAdded: string
+	dateModified: string
+	length: number
+	tags: string[]
+	_id: string
 }
 
-export default App;
+interface QuoteResponse {
+	quote: string
+	imageUrl: string
+	author: string
+}
+
+const getQuote: () => Promise<QuoteResponse> = async () => {
+	const response = await axios.get<any, AxiosResponse<Quote>>('https://api.quotable.io/random')
+	return {
+		quote: response.data.content,
+		author: response.data.author,
+		imageUrl: "https://source.unsplash.com/random/1920x1080"
+	}
+}
+
+function App() {
+	const [quote, setQuote] = useState<QuoteResponse>({
+		quote: '',
+		imageUrl: '',
+		author: ''
+	})
+
+	useEffect(() => {
+		getQuote().then(setQuote)
+	}, [])
+
+	return (
+		<div className='App' style={{ backgroundImage: `url(${quote.imageUrl})` }}>
+			<div className='Quote'>
+				<h1>
+					&ldquo; {quote.quote} &rdquo;
+				</h1>
+				<span className='Author'>{quote.author}</span>
+			</div>
+		</div>
+	)
+}
+
+export default App
